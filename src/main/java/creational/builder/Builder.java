32277ee6e1f90505a027ad6f4141bd0eebb5f6c7
nihan.guner <nihan.guner@n11.com>
2021-09-23 23:13:51 +0300

    Builder pattern added by lombok

---
+++ src/main/java/creational/builder/Builder.java
@@ -0,0 +1,18 @@
+package creational.builder;
+
+import lombok.Data;
+
+@Data
+@lombok.Builder
+public class Builder {
+    private int paramInt1;
+    private int paramInt2;
+    private int paramInt3;
+    private int paramInt4;
+    private int paramInt5;
+    private String paramString1;
+    private String paramString2;
+    private String paramString3;
+    private String paramString4;
+    private String paramString5;
+}
